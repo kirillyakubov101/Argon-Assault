18478066cf7bb83a8228896c53fdf30b7dc0b3a5
kirillyakubov <kirillyakubov101@gmail.com>
2020-05-28 23:49:52 -0400

    Setting Up the Project Lecture 10

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+	private void Awake()
+	{
+		DontDestroyOnLoad(this);
+	}
+
+	private void Start()
+	{
+		Invoke("LoadFirstScene",3f);
+	}
+
+	private void LoadFirstScene()
+	{
+		SceneManager.LoadScene(1);
+	}
+}
